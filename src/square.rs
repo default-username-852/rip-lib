9d056ba6890563244879a380f2a3fbb353356a43
Aron Bergman <bathingrad@gmail.com>
2019-09-17 22:17:30 +0200

    Make square print in standard format
    The square at (0,0) will print as A1

--- src/square.rs
+++ src/square.rs
@@ -15,3 +15,9 @@ impl Square {
 		}
 	}
 }
+
+impl std::fmt::Debug for Square {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		write!(f, "{}{}", self.file.as_char(), self.rank.as_char())
+	}
+}
