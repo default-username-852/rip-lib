87c8285d8ddaf45e640c8060d9b310560c0456b4
Aron Bergman <bathingrad@gmail.com>
2019-09-17 20:20:08 +0200

    Add function to return Vec with all names

--- src/name.rs
+++ src/name.rs
@@ -7,3 +7,16 @@ pub enum Name {
 	Knight,
 	Pawn,
 }
+
+impl Name {
+	pub fn all() -> Vec<Name> {
+		return vec![
+			Name::King,
+			Name::Queen,
+			Name::Rook,
+			Name::Bishop,
+			Name::Knight,
+			Name::Pawn
+		];
+	}
+}
